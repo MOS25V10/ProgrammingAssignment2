9a3a5498c57de509154c0feea39b1df13aab3ad1
MOS25V10 <wascroggins@gmail.com>
2015-04-26 16:02:16 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+NewVector <- function(x = numeric()) {
+        s <- NULL
+        set <- function(y) {
+                x <<- y
+                s <<- NULL
+        }
+get <- function() x
+setsum <- function(sum) s <<- sum
+getsum <- function() s
+list(set = set, get = get,
+        setsum = setsum,
+        getsum = getsum)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+cachesum <- function(x) {
+        s <- x$getsum()
+        if(!is.null(s)) {
+                message("pulling cached data")
+                return(s)
+        }
+        data <- x$get()
+        s <- sum(data)
+        x$setsum(s)
+        s
+}
         ## Return a matrix that is the inverse of 'x'
 }
